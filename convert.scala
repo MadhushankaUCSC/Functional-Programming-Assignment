a168274696c0b72d5a7ce9a1cea2046957fdfa5e
Madhushanka <65624662+MadhushankaUCSC@users.noreply.github.com>
2020-05-24 22:52:30 +0530

    Assignment1 Question1

---
+++ convert.scala
@@ -0,0 +1,12 @@
+object convert{
+def main(args:Array[String]){
+def toFahrenheit(x:Double):Double={
+x*1.8+32;
+}
+var fahrenheit=toFahrenheit(35);
+println("-----------------------------------------");
+println("Temperature in Fahrenheit :"+fahrenheit);
+println("-----------------------------------------");
+}
+
+}
